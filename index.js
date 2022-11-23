a12ee46a62b28e0343b32e79fc2fccd2d9aa2c8a
pawaters <pierre.alban.waters@gmail.com>
2022-11-23 14:34:55 +0200

    basis setup and install for front and back

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-
-const app = express ()
-
-app.listen(3005, () => {
-    console.log("Server running on port 3005")
-})
---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+
+const app = express ()
+
+app.listen(3001, () => {
+    console.log("Server running on port 3001")
+})
