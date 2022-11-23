62a80e3041f165aefde849e1db684479dfcdb762
pawaters <pierre.alban.waters@gmail.com>
2022-11-23 16:51:11 +0200

    added index.html

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const mysql = require('mysql2')
+const cors = require('cors');
+
+const db = mysql.createPool({
+	host: 'mysql_db', // the host name MYSQL_DATABASE: node_mysql
+	user: 'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
+	password: 'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
+	database: 'books' // database name MYSQL_HOST_IP: mysql_db
+})
+
 const app = express ()
 
 app.get('/', (request, response) => {
