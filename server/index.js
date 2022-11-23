9af4bd2f69d45ed625f3276a789819c2f96952d6
pawaters <pierre.alban.waters@gmail.com>
2022-11-23 15:51:47 +0200

    fron react app and back end express server working

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express")
 
 const app = express ()
 
+app.get('/', (request, response) => {
+    response.send('<h1>nodemon working?</h1>')
+  })
+
 app.listen(3001, () => {
     console.log("Server running on port 3001")
 })
