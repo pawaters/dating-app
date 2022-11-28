e5120b677528a54d7681ec42786f3bf598b31e11
pawaters <pierre.alban.waters@gmail.com>
2022-11-28 11:12:29 +0200

    css setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import React from "react"
+import "./style/App.css"
 
 function App() {
-    return <div className="App"> Front end </div>
+    return ( 
+        <div className="App"> 
+            <h1>SIGN UP TEST WITH SQL DB</h1> 
+
+            <div className="form">
+                <input type="text" name="First Name"/>
+                <input type="text" name="Last Name"/>
+            </div>
+        </div>
+    )
 }
  
 // REACT COMPONENTS TO DEFINE AND CREATE
