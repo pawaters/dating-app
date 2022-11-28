36a1d413793f72a89dd2a1b7959743e4675e118c
pawaters <pierre.alban.waters@gmail.com>
2022-11-28 11:05:43 +0200

    basic Front end and backend work - pending SQL db

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
-// Header 
-// Tinder cards
-// buttons below cards
-// chats screen
-// indiv chat screen
+import React from "react"
+
+function App() {
+    return <div className="App"> Front end </div>
+}
+ 
+// REACT COMPONENTS TO DEFINE AND CREATE
+    // Header 
+    // Tinder cards
+    // buttons below cards
+    // chats screen
+    // indiv chat screen
+
+export default App
