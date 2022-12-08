f1c9d1642c1a943f486cc5ad4b08133489d728b1
pawaters <pierre.alban.waters@gmail.com>
2022-12-08 14:52:05 +0200

    home UI v1, pending logo in nav

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Typography, withStyles } from '@mui/material'
 import Button from '@mui/material/button'
 import { Container } from '@mui/system'
 
@@ -6,6 +6,11 @@ const Home = () => {
 
     // all the variables needed before the return
     const authToken = false
+    // const WhiteTextTypography = withStyles({
+    //     root: {
+    //         color: "#FFFFFF",
+    //     }
+    // })
     
     const handleClick = () => {
         console.log('clicked')
@@ -13,15 +18,14 @@ const Home = () => {
 
     // after return, no logic, just the minimum, components
     return (
-        <>
-        {/* Background image */}
-        <Container maxWidth='sm' sx={{ pt:5, pb: 5}}>
-            <Typography variant='h1' sx={{ pt:5, pb:5 }}>Swipe Right®</Typography>
-            <Button variant="contained" size="large" onClick={handleClick}>
-                {authToken ? 'Signout' : 'Create Account'}
-            </Button>
-        </Container>
-        </>
+        <div style={{ backgroundImage: `url("https://assets.materialup.com/uploads/cd7deaa7-e263-4c1b-98c9-132d248fc0d4/preview.png")` }}>
+            <Container maxWidth='sm' sx={{ pt:5, pb: 5}}>
+                <Typography variant='h1' color={'black'} fontWeight={'bold'} sx={{ pt:5, pb:5 }}>Swipe Right®</Typography>
+                <Button variant="contained" size="large" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </Button>
+            </Container>
+        </div>
     )
 }
 
