6e3e59647975431837773a4f0aea1e2581197b20
pawaters <pierre.alban.waters@gmail.com>
2022-12-09 10:30:05 +0200

    first services defined & explanation

--- client/src/services/signUpService.js
+++ client/src/services/signUpService.js
@@ -1,8 +1,25 @@
+// in app.js, we have the whole single page application with the routes to the different pages
+// in each page, we defined the components
+// each component is rendered with some data, which is requested via these services (functions), 
+// services can also be used not only to get the data, but to transform it
+// these functions request their data from API endpoint
+// each endpoint is made to return "response.data" with a specific format
+
 import axios from 'axios'
 const baseUrl = '/api'
 
 const createUser = signedUpUser => {
 	const request = axios.post(`${baseUrl}/signup`, signedUpUser)
+	return request.then(response => response.data)
+}
+
+const verifyUser = userToVerify => {
+	const request = axios.post(`${baseUrl}/signup/verifyuser`, userToVerify)
+	return request.then(response => response.data)
+}
+
+const logInUser = signedUpUser => {
+	const request = axios.post(`${baseUrl}/login`, signedUpUser)
 	return request.then(response => response.data)
 }
 
