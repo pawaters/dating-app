cd1d73d3d78f1187ca0d9e2ca3755a3b0797edce
pawaters <pierre.alban.waters@gmail.com>
2022-12-09 10:34:40 +0200

    sign up services established

--- client/src/services/signUpService.js
+++ client/src/services/signUpService.js
@@ -23,3 +23,26 @@ const logInUser = signedUpUser => {
 	return request.then(response => response.data)
 }
 
+const getSessionUser = () => {
+	const request = axios.get(`${baseUrl}/login`)
+	return request.then(response => response.data) 
+}
+
+const logOutUser = () => {
+	const request = axios.get(`${baseUrl}/logout`)
+	return request.then(response => response.data) 
+}
+
+const resetPassword = resetInfo => {
+	const request = axios.post(`${baseUrl}/resetpassword`, resetInfo)
+	return request.then(response => response.data)
+}
+
+const setNewPassword = passwords => {
+	const request = axios.post(`${baseUrl}/setnewpassword`, passwords)
+	return request.then(response => response.data)
+}
+
+const signUpService = { createUser, verifyUser, logInUser, logOutUser, getSessionUser, resetPassword, setNewPassword}
+
+export default signUpService
