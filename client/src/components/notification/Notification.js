d545104e1397c7dc16a02262846afda4f92c9933
pawaters <pierre.alban.waters@gmail.com>
2022-12-15 15:53:43 +0200

    WIP notificationReducer

--- client/src/components/notification/Notification.js
+++ client/src/components/notification/Notification.js
@@ -1,12 +1,12 @@
 import { Alert } from "@mui/material"
 import { useSelector } from "react-redux"
 
-const notification = useSelector(state => state.notification)
+// const notification = useSelector(state => state.notification)
 
 const Notification = () => {
     return (
         <Alert>
-            {notification}
+            {/* {notification} */}
         </Alert>
     )
 }
