21e97472bdb1f87ca8b3ee6f2ad6d264d43a7532
pawaters <pierre.alban.waters@gmail.com>
2022-12-16 12:22:09 +0200

    login WIP

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,9 +1,75 @@
+import { Button, Paper, TextField, Typography } from "@mui/material"
 import { Container } from "@mui/system"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Navigate, useNavigate } from "react-router-dom"
+import Notification from "../../components/notification/Notification"
+import { changeNotification } from "../../reducers/notificationReducer"
+import { changeSeverity } from "../../reducers/severityReducer"
+import signUpService from "../../services/signUpService"
+
+
 
 const Login = () => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const user = useSelector(state => state.user)
+
+    useEffect( () => {
+        if (user !== undefined && user !== '') {
+            navigate('/profile')
+        }
+    }, [user, navigate])
+
+    const submitUser = async (event) => {
+        event.preventDefault()
+    
+        const signedUpUser = {
+            username: event.target.username.value,
+            password: event.target.password.value,
+        }
+        // console.log('user:', user,'empty space right before that?')
+        // console.log(signedUpUser)
+
+        signUpService.logInUser(signedUpUser)
+            .then(result => {
+                
+                if (result.userid) {
+                    const sessionUser = { name: result.username, id: result.userid}
+                    dispatch(changeSeverity('success'))
+                    dispatch(changeNotification('User created successfully (PENDING EMAIL CONFIRMATION SETUP'))
+                    navigate('/login')
+                } else {
+                    dispatch(changeSeverity('error'))
+                    dispatch(changeNotification(result))
+                }
+            })
+    }
+
     return (
-        <Container >
-            <h1>Login</h1>
+        <Container 
+        sx={{ 
+            pt:5, 
+            pb: 5,
+            backgroundImage: `url("https://assets.materialup.com/uploads/cd7deaa7-e263-4c1b-98c9-132d248fc0d4/preview.png")`,
+            backgroundSize: 'cover',
+            width: 'auto',
+            height: 'auto',
+            minHeight: '80vh',
+        }}
+        >
+            <Paper elevation={10} sx={{ padding: 3, width:'50%', margin: 'auto'}} >
+                <Typography variant="h2"> Login </Typography>
+                <form onSubmit={submitUser}>
+                    <TextField fullWidth margin='normal' name='username' label='Username' placeholder="Username" autoComplete="nickname" required> </TextField>
+                    <TextField fullWidth margin='normal' type="password" name='password' label='Password' placeholder="Password" autoComplete="new-password" required> </TextField>
+                    <Button type='submit' variant="contained" size="large" sx={{ mt: 2 }}> Submit </Button>
+                </form>
+                <Button> Forgot Password?</Button>
+                <Notification />
+            </Paper>
         </Container>
     )
 }
