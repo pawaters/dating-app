f1a434fbed45527f10bce44a01c674bef989d6e1
pawaters <pierre.alban.waters@gmail.com>
2022-12-16 16:55:17 +0200

    end of day!

--- server/src/db/database.sql
+++ server/src/db/database.sql
@@ -1,7 +1,13 @@
 CREATE DATABASE matcha;
 
-CREATE TABLE users(
-    users_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL NOT NULL PRIMARY KEY,
+	username VARCHAR(255) NOT NULL,
+	firstname VARCHAR(255) NOT NULL,
+	lastname VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	verified enum_yesno DEFAULT 'NO',
+	last_connection TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- (pending make columns not null )
