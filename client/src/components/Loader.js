f523f59f8f510e93eecd63dd1d34f5bfb5092e30
pawaters <pierre.alban.waters@gmail.com>
2022-12-20 16:01:56 +0200

    profile WIP

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material"
+import { Container, Typography } from "@mui/material"
 import { Box } from "@mui/system"
 import React from "react"
 
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,12 +1,42 @@
-import * as React from 'react';
+import { Avatar, Grid, Paper } from "@mui/material"
+import { Box, Container } from "@mui/system"
+import { useEffect, useState } from "react"
 import { AspectRatio } from 'react-aspect-ratio'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate} from 'react-router-dom'
+// import { getProfileData } from '../reducers/profileReducer.js'
 
 const Profile = () => {
+    const [isLoading, setLoading] = useState(true)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const profileData = useSelector(state => state.profile)
+
+    useEffect(() => {
+        const getData = async () => {
+            // await dispatch(getProfileData())
+            setLoading(false)
+        }
+    }, [dispatch])
+
     return (
-        <div>
-            <AspectRatio></AspectRatio>
-        </div>
-    );
+        <Container>
+            <Paper>
+                <Grid>
+                    <Box>
+                        <AspectRatio>
+                            <Avatar 
+                                // src={profile_pic}
+                                alt='profile'
+                            
+                            />
+                        </AspectRatio>
+                    </Box>
+                </Grid>
+            </Paper>
+        </Container>
+    )
+
 }
 
 export default Profile
