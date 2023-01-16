e01c56a39e32565bcb6ef9db344c0eb1f0f50fd7
pawaters <pierre.alban.waters@gmail.com>
2023-01-16 15:40:57 +0200

    store updated

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,7 +3,13 @@ import userReducer from '../reducers/userReducer'
 import notificationReducer from '../reducers/notificationReducer'
 import severityReducer from '../reducers/severityReducer'
 import profileReducer from '../reducers/profileReducer'
-
+// import onlineUsersReducer from '../reducers/onlineUsersReducer'
+import browsingReducer from '../reducers/browsingReducer'
+import userListsReducer from '../reducers/userListsReducer'
+import displaySettingsReducer from '../reducers/displaySettingsReducer'
+// import roomReducer from '../reducers/roomReducer'
+// import messagesReducer from '../reducers/messagesReducer'
+import userNotificationsReducer from '../reducers/userNotificationsReducer'
 
 const store = configureStore({
 	reducer: {
@@ -11,6 +17,13 @@ const store = configureStore({
 		notification: notificationReducer,
 		severity: severityReducer,
 		profile: profileReducer,
+		// onlineUsers: onlineUsersReducer,
+		browsingCriteria: browsingReducer,
+		displaySettings: displaySettingsReducer,
+		userLists: userListsReducer,
+		// room: roomReducer,
+		// messages: messagesReducer,
+		userNotifications: userNotificationsReducer,
 	}
 })
 
