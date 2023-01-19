e5e855008f32fbf00ddbdf45901a38ceb10133d5
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-01-19 17:17:18 +0200

    Made a correction to the function that returns the distance between users. Implemented password change in backend.

--- server/src/routes/tableSetup.js
+++ server/src/routes/tableSetup.js
@@ -202,14 +202,14 @@ module.exports = function (app, pool, bcrypt) {
                 		theta float;
                 		radtheta float;
                 	BEGIN
-                		IF lat1 = lat2 OR lon1 = lon2
-                			THEN RETURN dist;
+                		IF lat1 = lat2 AND lon1 = lon2
+                			THEN RETURN dist    ;
                 		ELSE
                 			radlat1 = pi() * lat1 / 180;
                 			radlat2 = pi() * lat2 / 180;
                 			theta = lon1 - lon2;
                 			radtheta = pi() * theta / 180;
-                			dist = sin(radlat1) * sin(radlat2) + cos(radlat1) * cos(radlat2) * cos(radtheta);
+                			dist = (sin(radlat1) * sin(radlat2)) + (cos(radlat1) * cos(radlat2) * cos(radtheta));
                                 
                 			IF dist > 1 THEN dist = 1; END IF;
                                 
@@ -227,8 +227,6 @@ module.exports = function (app, pool, bcrypt) {
                 `, "Creation of the GPS distance function"),
             ])
         }
-
-        
 
         createTables()
             .then(() => {
